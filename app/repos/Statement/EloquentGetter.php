d923e193a70eff2c4d9c077f79e9a6cfe4199a34
Ryan Smith <0ryansmith1994@gmail.com>
2015-01-29 11:28:06 +0000

    Comments some code.

--- app/repos/Statement/EloquentGetter.php
+++ app/repos/Statement/EloquentGetter.php
@@ -17,8 +17,10 @@ class EloquentGetter implements GetterInterface {
   const DEFAULT_LIMIT = 100;
 
   public function aggregate(Authority $authority, array $pipeline) {
+    // Stops people using the $out operator.
     if (strpos(json_encode($pipeline), '$out') !== false) return;
 
+    // Ensures that you can't get statements from other LRSs.
     $pipeline[0] = array_merge_recursive([
       '$match' => ['lrs._id' => $authority->getLRS()]
     ], $pipeline[0]);
@@ -27,15 +29,18 @@ class EloquentGetter implements GetterInterface {
   }
 
   public function index(Authority $authority, array $options) {
+    // Gets and validates options.
     $options = $this->getIndexOptions($options);
     $this->validateIndexOptions($options);
 
+    // Constructs pipelines and aggregates statements.
     $index_pipeline = $this->constructIndexPipeline($options);
     $statements_pipeline = $this->projectLimitedStatements($index_pipeline, $options);
     $count_pipeline = $this->projectCountedStatements($index_pipeline);
     $statements = $this->aggregate($authority, $statements_pipeline)['result'];
     $count = $this->aggregate($authority, $count_pipeline)['result'][0]['count'];
 
+    // Formats statements.
     switch ($options['format']) {
       case 'exact': $statements = $statements; break;
       case 'ids': $statements = (new EloquentFormatter)->toIds($statements); break;
